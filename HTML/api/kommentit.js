aab27411458a793feff91859f498bb5e42b6e23b
peikkoKoodaa <toivo.i.rytkonen@gmail.com>
2026-04-19 09:50:58 +0300

    Update kommentit.js

--- HTML/api/kommentit.js
+++ HTML/api/kommentit.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       const { data, error } = await supabase
         .from('kommentit')
         .select('*')
-        .limit(res.limit)
+        .limit(req.limit)
         .order('id', { ascending: false });
 
       if (error) {
